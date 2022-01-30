9f3fe2bf13a19e98c32212523c9cff96abfbdb26
Andrew Mitchell <ajx256@gmail.com>
2022-01-30 16:04:12 -0500

    Speeds up path plotting using LineCollections

--- visualization/pathVisualizer.py
+++ visualization/pathVisualizer.py
@@ -5,6 +5,7 @@ from mpl_toolkits import mplot3d
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation  as animation
 
 import math
 import sys
@@ -13,15 +14,15 @@ def on_close(event):
     plt.close()
     exit()
 
-def plot2DPoint(x, y):
-    plt.plot([x], [y], 'ro')
-    #plt.pause(0.001)
+def plot2DPoint(x, y, points):
+    points[0].append(x)
+    points[1].append(y)
+    points[2].append(0.0)
     
-def plot2DLine(x1, y1, x2, y2, color):
-    plt.plot([x1, x2], [y1, y2], color)
-    #plt.pause(0.001)
+def plot2DLine(x1, y1, x2, y2, lines):
+    lines.append([(x1, y1, 0.0), (x2, y2, 0.0)])
 
-def plot2DCurve(x1, y1, speedX, speedY, steeringAngle, angularAcc, color):
+def plot2DCurve(x1, y1, speedX, speedY, steeringAngle, angularAcc, lines):
     theta = steeringAngle
     s = angularAcc
     x = x1
@@ -36,12 +37,43 @@ def plot2DCurve(x1, y1, speedX, speedY, steeringAngle, angularAcc, color):
         dy = dy + (dt * s * math.sin(thetaTot))
         x = x + (dt * dx)
         y = y + (dt * dy)
-        plt.plot([x1, x], [y1, y], color)
+        lines.append([(x1, y1, 0.0), (x, y, 0.0)])
         x1 = x
         y1 = y
-    #plt.pause(0.001)
 
 try:
+    # Read in from stdin
+    solution = []
+    treeLines = []
+    trajectoryLines = []
+    solutionPoints = [[], [], []]
+    readingSolution = False
+    # Build up the lines for the search tree, and set aside the solution points for later...
+    for line in sys.stdin:
+        vals = line.rstrip().split(' ')
+        if len(vals) == 1 and not readingSolution:
+            readingSolution = True
+        elif len(vals) == 1 and readingSolution:
+            readingSolution = False
+        elif len(vals) > 1 and readingSolution:
+            solution.append(vals)
+        elif not readingSolution and len(vals) == 4:
+            plot2DLine(float(vals[0]), float(vals[1]), float(vals[2]), float(vals[3]), treeLines)
+        elif not readingSolution and len(vals) == 6:
+            plot2DCurve(float(vals[0]), float(vals[1]), float(vals[2]), float(vals[3]), float(vals[4]), float(vals[5]), treeLines)
+
+    # Build up both the solution trajectory as well as the points along it...
+    lastNode = []
+    for node in solution:
+        if len(node) == 2:
+            plot2DPoint(float(node[0]), float(node[1]), solutionPoints)
+            if len(lastNode) != 0:
+                plot2DLine(float(lastNode[0]), float(lastNode[1]), float(node[0]), float(node[1]), trajectoryLines)
+            lastNode = node
+        if len(node) == 6:
+            plot2DPoint(float(node[0]), float(node[1]), solutionPoints)
+            plot2DCurve(float(node[0]), float(node[1]), float(node[2]), float(node[3]), float(node[4]), float(node[5]), trajectoryLines)
+
     # Define plot
     fig = plt.figure()
     fig.canvas.mpl_connect('close_event', on_close)
@@ -52,37 +84,9 @@ try:
     ax.xaxis.set_ticks(np.arange(0, 10, 1))
     ax.yaxis.set_ticks(np.arange(0, 10, 1))
     ax.zaxis.set_ticks(np.arange(0, 10, 1))
-
-    # Read in from stdin
-    trajectoryNodes = []
-    readingSolution = False
-    for line in sys.stdin:
-        vals = line.rstrip().split(' ')
-        if len(vals) == 1 and not readingSolution:
-            readingSolution = True
-        elif len(vals) == 1 and readingSolution:
-            readingSolution = False
-
-        if len(vals) > 1 and readingSolution:
-            trajectoryNodes.append(vals)
-        if not readingSolution and len(vals) == 4:
-            plot2DLine(float(vals[0]), float(vals[1]), float(vals[2]), float(vals[3]), '-k')
-            #print("GOT LINE: (" + vals[0] + "," + vals[1] + ") -> (" + vals[2] + "," + vals[3] + ")")
-        if not readingSolution and len(vals) == 6:
-            #print("GOT CURVE: " + line)
-            plot2DCurve(float(vals[0]), float(vals[1]), float(vals[2]), float(vals[3]), float(vals[4]), float(vals[5]), '-k')
-
-    lastNode = []
-    for node in trajectoryNodes:
-        if len(node) == 2:
-            plot2DPoint(float(node[0]), float(node[1]))
-            #print("GOT TRAJECTORY POINT: (" + node[0] + "," + node[1] + ")")
-            if len(lastNode) != 0:
-                plot2DLine(float(lastNode[0]), float(lastNode[1]), float(node[0]), float(node[1]), '-r')
-            lastNode = node
-        if len(node) == 6:
-            plot2DPoint(float(node[0]), float(node[1]))
-            plot2DCurve(float(node[0]), float(node[1]), float(node[2]), float(node[3]), float(node[4]), float(node[5]), '-r')
+    
+    ax.add_collection(mplot3d.art3d.Line3DCollection(treeLines, color='k', lw=1))
+    ax.add_collection(mplot3d.art3d.Line3DCollection(trajectoryLines, color='r', lw=3))
 
     plt.show()
 except:
